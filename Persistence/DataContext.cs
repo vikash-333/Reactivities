14c8e8d7bc4d0f1d60890f587b241d8ef8133f0a
Vikash Kumar <kumarvikash148@gmail.com>
2021-02-17 23:54:07 +0530

    first commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        private readonly DbContextOptions _options;
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            _options = options;
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
