0e75f914faafcd0e54e165ea0e8ac44c1e7abb82
Vikash Kumar <kumarvikash148@gmail.com>
2021-02-21 20:42:36 +0530

    End of section 4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -18,10 +18,30 @@ namespace API.Controllers
             return await Mediator.Send(new List.Query());
         }
 
-        /*     [HttpGet("{id}")]
-            public async Task<ActionResult<Activity>> GetActivity(Guid id)
-            {
-               // return await _context.Activities.FindAsync(id);
-            } */
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+                return await Mediator.Send(new Details.Query(id));
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+             return Ok(await Mediator.Send(new Create.Command{Activity = activity}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id=id;
+            return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command {Id=id}));
+        }
     }
 }
