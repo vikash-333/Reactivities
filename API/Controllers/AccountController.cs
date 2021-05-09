8b1f865e5b2491266d2ed76ac31e3cc62eeefdda
Vikash Kumar <kumarvikash148@gmail.com>
2021-05-09 11:34:58 +0530

    End of section 16 - API image upload

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using API.DTOs;
@@ -30,7 +31,8 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
 
-            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            var user = await _userManager.Users.Include(p => p.Photos)
+                .FirstOrDefaultAsync(x=> x.Email == loginDto.Email);
             if (user == null) return Unauthorized();
 
             var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
@@ -79,7 +81,8 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<UserDto>> CurrentUser()
         {
-            var user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+            var user = await _userManager.Users.Include(p => p.Photos)
+                .FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
             return CreateUserObject(user);
         }
 
@@ -89,7 +92,7 @@ namespace API.Controllers
                 {
                     DisplayName = user.DisplayName,
                     UserName = user.UserName,
-                    Image = null,
+                    Image = user?.Photos?.FirstOrDefault(x => x.IsMain).Url,
                     Token = _tokenService.CreateToken(user)
                 };
         }
---
+++ Application/Profiles/Details.cs
@@ -0,0 +1,39 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Profiles
+{
+    public class Details
+    {
+        public class Query : IRequest<Result<Profile>>
+        {
+            public string UserName { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result<Profile>>
+        {
+            private readonly IMapper _mapper;
+            private readonly DataContext _context;
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<Result<Profile>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var user = await _context.Users
+                    .ProjectTo<Profile>(_mapper.ConfigurationProvider)
+                    .SingleOrDefaultAsync(x => x.Username == request.UserName);
+
+                return Result<Profile>.Success(user);
+            }
+        }
+    }
+}
